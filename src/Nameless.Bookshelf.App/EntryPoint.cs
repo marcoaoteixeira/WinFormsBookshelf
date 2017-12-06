d4b6ac305556f2043941a1b3f14a9e8e75c77c5f
marcoaoteixeira <marcoaoteixeira@gmail.com>
2017-12-06 17:07:26 -0200

    Initial commit

---
+++ src/Nameless.Bookshelf.App/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using Nameless.Bookshelf.Views;
+
+namespace Nameless.Bookshelf {
+
+    internal static class EntryPoint {
+
+        #region Internal Static Methods
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        internal static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+        #endregion Internal Static Methods
+    }
+}
---
+++ src/Nameless.Bookshelf.App/Views/MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Nameless.Bookshelf.Views {
+
+    public partial class MainForm : Form {
+
+        #region Public Constructors
+        public MainForm() {
+            InitializeComponent();
+        } 
+        #endregion
+    }
+}
